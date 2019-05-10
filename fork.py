d35f076c58f0510a4eab75fb6c4179bb2ceb3a1e
HCLivess <admin@bismuth.cz>
2019-05-10 15:47:15 -0500

    split fork2 into a separate class

--- fork.py
+++ fork.py
@@ -11,3 +11,13 @@ class Fork():
         if 'mainnet0018' in node.version_allow:
             node.logger.app_log.warning(f"Beginning to reject mainnet0018 - block {node.last_block}")
             node.version_allow.remove('mainnet0018')
+
+class Fork2():
+    def __init__(self):
+        self.POW_FORK = 1200000
+        self.FORK_AHEAD = 5
+
+    def limit_version(self, node):
+        if 'mainnet0019' in node.version_allow:
+            node.logger.app_log.warning(f"Beginning to reject mainnet0019 - block {node.last_block}")
+            node.version_allow.remove('mainnet0019')
