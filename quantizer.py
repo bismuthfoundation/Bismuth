eecd3b7b6872bf561a2a57d3284500e0e3b7ef76
b-i-z <50231809+b-i-z@users.noreply.github.com>
2019-07-18 09:45:41 +0100

    quantize_two/eight/ten optimization, because it's heavily used in certain places

--- quantizer.py
+++ quantizer.py
@@ -1,16 +1,20 @@
 from decimal import *
 
+DECIMAL_ZERO_2DP = Decimal('0.00')
+DECIMAL_ZERO_8DP = Decimal('0.00000000')
+DECIMAL_ZERO_10DP = Decimal('0.0000000000')
+
 def quantize_two(value):
     value = Decimal(value)
-    value = value.quantize(Decimal('0.00'))
+    value = value.quantize(DECIMAL_ZERO_2DP)
     return value
 
 def quantize_eight(value):
     value = Decimal(value)
-    value = value.quantize(Decimal('0.00000000'))
+    value = value.quantize(DECIMAL_ZERO_8DP)
     return value
 
 def quantize_ten(value):
     value = Decimal(value)
-    value = value.quantize(Decimal('0.0000000000'))
+    value = value.quantize(DECIMAL_ZERO_10DP)
     return value
