1978e462c2839e400246f1a9c1a49cb6b52e3b7a
EggPool <dev@EggPool.net>
2019-08-04 11:00:59 +0200

    Handle case of None value for empty addresses when digesting

--- quantizer.py
+++ quantizer.py
@@ -4,17 +4,27 @@ DECIMAL_ZERO_2DP = Decimal('0.00')
 DECIMAL_ZERO_8DP = Decimal('0.00000000')
 DECIMAL_ZERO_10DP = Decimal('0.0000000000')
 
+
 def quantize_two(value):
+    if not value:
+        return DECIMAL_ZERO_2DP
     value = Decimal(value)
     value = value.quantize(DECIMAL_ZERO_2DP)
     return value
 
+
 def quantize_eight(value):
+    if not value:
+        # Will match 0 as well as False and None
+        return DECIMAL_ZERO_8DP
     value = Decimal(value)
     value = value.quantize(DECIMAL_ZERO_8DP)
     return value
 
+
 def quantize_ten(value):
+    if not value:
+        return DECIMAL_ZERO_10DP
     value = Decimal(value)
     value = value.quantize(DECIMAL_ZERO_10DP)
     return value
