f0adeba4e5d953c1fe84a5b74ee1924306b16d43
geho2 <26554545+geho2@users.noreply.github.com>
2020-07-02 08:36:41 +0200

    New test: spend entire balance

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -60,3 +60,18 @@ def test_function4(): #Operation and openfield
     time.sleep(1)
     tx = client.latest_transactions(num=1)
     assert (tx[0]["operation"] == operation) and (tx[0]["openfield"] == data)
+
+def test_function5(): #Spend entire balance
+    client = BismuthClient(servers_list={'127.0.0.1:3030'},wallet_file='../datadir/wallet.der')
+    client.command(command="regtest_generate",options=[1]) #Mine a block so we have some funds
+    client.clear_cache()
+    balance = float(client.balance())
+    fee = 0.01
+    recipient = "8342c1610de5d7aa026ca7ae6d21bd99b1b3a4654701751891f08742"
+    txid = client.send(recipient=recipient, amount=balance-fee)
+    client.command(command="regtest_generate",options=[1]) #Mine the next block
+    time.sleep(1)
+    client.clear_cache()
+    balance = float(client.balance())
+    tx = client.latest_transactions(num=2)
+    assert abs(balance - float(tx[1]["reward"])) < 1e-6
