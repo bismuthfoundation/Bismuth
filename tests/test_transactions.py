156cc1daf495d329317d09c09bd61916489e1506
geho2 <geho2@bismuth.live>
2020-07-02 16:05:23 +0200

    New test: spend more than owned

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -75,3 +75,16 @@ def test_function5(): #Spend entire balance
     balance = float(client.balance())
     tx = client.latest_transactions(num=2)
     assert abs(balance - float(tx[1]["reward"])) < 1e-6
+
+def test_function6(): #Send more than owned
+    client = BismuthClient(servers_list={'127.0.0.1:3030'},wallet_file='../datadir/wallet.der')
+    client.command(command="regtest_generate",options=[1]) #Mine a block so we have some funds
+    client.clear_cache()
+    balance = float(client.balance())
+    recipient = "8342c1610de5d7aa026ca7ae6d21bd99b1b3a4654701751891f08742"
+    txid = client.send(recipient=recipient, amount=balance)
+    client.command(command="regtest_generate",options=[1]) #Mine the next block
+    time.sleep(1)
+    client.clear_cache()
+    balance = float(client.balance())
+    assert (balance>1.0)
