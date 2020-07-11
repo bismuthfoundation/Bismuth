aec7e95c8e1b2161dc4c30cb4b3378e5d994330f
geho2 <geho2@bismuth.live>
2020-07-11 22:20:00 +0200

    More ledger and node tests

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -11,3 +11,11 @@ def test_blocklast_json(myserver):
     data1 = client.command(command="blocklast")
     data2 = client.command(command="blocklastjson")
     assert (int(data1[0]) == data2['block_height']) and (data1[7] == data2['block_hash'])
+
+def test_balance_json(myserver):
+    client = BismuthClient(servers_list={'127.0.0.1:3030'},wallet_file='../datadir/wallet.der')
+    data1 = client.command(command="balanceget", options=[client.address])
+    data2 = client.command(command="balancegetjson", options=[client.address])
+    assert data1[0] == data2['balance'] and data1[1] == data2['credit'] and \
+           data1[2] == data2['debit'] and data1[3] == data2['fees'] and \
+           data1[4] == data2['rewards'] and data1[5] == data2['balance_no_mempool']
--- tests/test_node.py
+++ tests/test_node.py
@@ -20,3 +20,10 @@ def test_diff_json(myserver):
     block2 = data2['block']
     diff2 = data2['difficulty']
     assert (block1 == block2) and (diff1 == diff2)
+
+def test_keygen_json(myserver):
+    client = BismuthClient(servers_list={'127.0.0.1:3030'},wallet_file='../datadir/wallet.der')
+    data1 = client.command(command="keygen")
+    data2 = client.command(command="keygenjson")
+    assert len(data1[1]) == len(data2['public_key']) and \
+           len(data1[2]) == len(data2['address'])
