51032436c514ed21ab690ec1c2116139bc64ca10
alias-bitsignal <39199608+alias-bitsignal@users.noreply.github.com>
2025-07-02 11:55:33 +0200

    Update node.py  V2 version to 5.1.01-evo
    significant change removing dev_rewards & hn_rewards distribution

--- node.py
+++ node.py
@@ -38,7 +38,7 @@ from libs.node import Node
 from libs.nodebackgroundthread import NodeBackgroundThread
 
 
-VERSION = "5.0.47-evo"  # Experimental db-evolution branch
+VERSION = "5.1.01-evo"  # Experimental db-evolution branch
 
 
 appname = "Bismuth"
