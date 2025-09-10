1b14bb4d5f0e25659facce94352a851af7be328f
hclivess <admin@bismuth.cz>
2025-09-10 23:02:27 +0200

    removing disgusting globals from functions and separating

--- node.py
+++ node.py
@@ -1,17 +1,7 @@
-# c = hyperblock in ram OR hyperblock file when running only hyperblocks without ram mode on
-from utils import blocknf, sequencing_check, sql_trace_callback
+from utils import blocknf, sequencing_check, sql_trace_callback, bootstrap, check_integrity, balanceget, \
+    ledger_check_heights, recompress_ledger
 
-# h = ledger file or hyperblock clone in hyperblock mode
-# h2 = hyperblock file
 from utils import add_indices, verify
-
-# never remove the str() conversion in data evaluation or database inserts or you will debug for 14 days as signed types mismatch
-# if you raise in the server thread, the server will die and node will stop
-# never use codecs, they are bugged and do not provide proper serialization
-# must unify node and client now that connections parameters are function parameters
-# if you have a block of data and want to insert it into sqlite, you must use a single "commit" for the whole batch, it's 100x faster
-# do not isolation_level=None/WAL hdd levels, it makes saving slow
-# issues with db? perhaps you missed a commit() or two
 
 
 VERSION = "4.5.0.1"
@@ -41,7 +31,7 @@ import plugins
 import wallet_keys
 from connections import send, receive
 from digest import *
-from essentials import fee_calculate, download_file
+from essentials import download_file
 from libs import node, logger, keys, client
 from fork import Fork
 
@@ -59,240 +49,6 @@ appauthor = "Bismuth Foundation"
 
 # nodes_ban_reset=config.nodes_ban_reset
 
-
-def bootstrap():
-    # TODO: Candidate for single user mode
-    try:
-        types = ['static/*.db-wal', 'static/*.db-shm']
-        for t in types:
-            for f in glob.glob(t):
-                os.remove(f)
-                print(f, "deleted")
-
-        archive_path = node.ledger_path + ".tar.gz"
-        download_file("https://bismuth.cz/ledger.tar.gz", archive_path)
-
-        with tarfile.open(archive_path) as tar:
-            tar.extractall("static/")  # NOT COMPATIBLE WITH CUSTOM PATH CONFS
-
-    except:
-        node.logger.app_log.warning("Something went wrong during bootstrapping, aborted")
-        raise
-
-
-def check_integrity(database):
-    # TODO: Candidate for single user mode
-    # check ledger integrity
-
-    if not os.path.exists("static"):
-        os.mkdir("static")
-
-    with sqlite3.connect(database) as ledger_check:
-        if node.trace_db_calls:
-            ledger_check.set_trace_callback(functools.partial(sql_trace_callback, node.logger.app_log, "CHECK_INTEGRITY"))
-
-        ledger_check.text_factory = str
-        l = ledger_check.cursor()
-
-        try:
-            l.execute("PRAGMA table_info('transactions')")
-            redownload = False
-        except:
-            redownload = True
-
-        if len(l.fetchall()) != 12:
-            node.logger.app_log.warning(
-                f"Status: Integrity check on database {database} failed, bootstrapping from the website")
-            redownload = True
-
-    if redownload and node.is_mainnet:
-        bootstrap()
-
-
-def rollback(node, db_handler, block_height):
-    node.logger.app_log.warning(f"Status: Rolling back below: {block_height}")
-
-    db_handler.rollback_under(block_height)
-
-    # rollback indices
-    db_handler.tokens_rollback(node, block_height)
-    db_handler.aliases_rollback(node, block_height)
-    # rollback indices
-
-    node.logger.app_log.warning(f"Status: Chain rolled back below {block_height} and will be resynchronized")
-
-
-def recompress_ledger(node, rebuild=False, depth=15000):
-    # TODO: Candidate for single user mode
-    node.logger.app_log.warning(f"Status: Recompressing, please be patient")
-
-    files_remove = [node.ledger_path + '.temp',node.ledger_path + '.temp-shm',node.ledger_path + '.temp-wal']
-    for file in files_remove:
-        if os.path.exists(file):
-            os.remove(file)
-            node.logger.app_log.warning(f"Removed old {file}")
-
-    if rebuild:
-        node.logger.app_log.warning(f"Status: Hyperblocks will be rebuilt")
-
-        shutil.copy(node.ledger_path, node.ledger_path + '.temp')
-        hyper = sqlite3.connect(node.ledger_path + '.temp')
-    else:
-        shutil.copy(node.hyper_path, node.ledger_path + '.temp')
-        hyper = sqlite3.connect(node.ledger_path + '.temp')
-    if node.trace_db_calls:
-       hyper.set_trace_callback(functools.partial(sql_trace_callback, node.logger.app_log, "HYPER"))
-    hyper.text_factory = str
-    hyp = hyper.cursor()
-
-    hyp.execute("UPDATE transactions SET address = 'Hypoblock' WHERE address = 'Hyperblock'")
-
-    hyp.execute("SELECT max(block_height) FROM transactions")
-    db_block_height = int(hyp.fetchone()[0])
-    depth_specific = db_block_height - depth
-
-    hyp.execute(
-        "SELECT distinct(recipient) FROM transactions WHERE (block_height < ? AND block_height > ?) ORDER BY block_height;",
-        (depth_specific, -depth_specific,))  # new addresses will be ignored until depth passed
-    unique_addressess = hyp.fetchall()
-
-    for x in set(unique_addressess):
-        credit = Decimal("0")
-        for entry in hyp.execute(
-                "SELECT amount,reward FROM transactions WHERE recipient = ? AND (block_height < ? AND block_height > ?);",
-                (x[0],) + (depth_specific, -depth_specific,)):
-            try:
-                credit = quantize_eight(credit) + quantize_eight(entry[0]) + quantize_eight(entry[1])
-                credit = 0 if credit is None else credit
-            except Exception:
-                credit = 0
-
-        debit = Decimal("0")
-        for entry in hyp.execute(
-                "SELECT amount,fee FROM transactions WHERE address = ? AND (block_height < ? AND block_height > ?);",
-                (x[0],) + (depth_specific, -depth_specific,)):
-            try:
-                debit = quantize_eight(debit) + quantize_eight(entry[0]) + quantize_eight(entry[1])
-                debit = 0 if debit is None else debit
-            except Exception:
-                debit = 0
-
-        end_balance = quantize_eight(credit - debit)
-
-        if end_balance > 0:
-            timestamp = str(time.time())
-            hyp.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", (
-                depth_specific - 1, timestamp, "Hyperblock", x[0], str(end_balance), "0", "0", "0", "0",
-                "0", "0", "0"))
-    hyper.commit()
-
-    hyp.execute(
-        "DELETE FROM transactions WHERE address != 'Hyperblock' AND (block_height < ? AND block_height > ?);",
-        (depth_specific, -depth_specific,))
-    hyper.commit()
-
-    hyp.execute("DELETE FROM misc WHERE (block_height < ? AND block_height > ?);",
-                (depth_specific, -depth_specific,))  # remove diff calc
-    hyper.commit()
-
-    hyp.execute("VACUUM")
-    hyper.close()
-
-    if os.path.exists(node.hyper_path):
-        os.remove(node.hyper_path)  # remove the old hyperblocks to rebuild
-        os.rename(node.ledger_path + '.temp', node.hyper_path)
-
-
-def ledger_check_heights(node, db_handler):
-    # TODO: Candidate for single user mode
-    """conversion of normal blocks into hyperblocks from ledger.db or hyper.db to hyper.db"""
-    if os.path.exists(node.hyper_path):
-
-        # cross-integrity check
-        hdd_block_max = db_handler.block_height_max()
-        hdd_block_max_diff = db_handler.block_height_max_diff()
-        hdd2_block_last = db_handler.block_height_max_hyper()
-        hdd2_block_last_misc = db_handler.block_height_max_diff_hyper()
-
-        # cross-integrity check
-
-        if hdd_block_max == hdd2_block_last == hdd2_block_last_misc == hdd_block_max_diff and node.hyper_recompress:  # cross-integrity check
-            node.logger.app_log.warning("Status: Recompressing hyperblocks (keeping full ledger)")
-            node.recompress = True
-
-            #print (hdd_block_max,hdd2_block_last,node.hyper_recompress)
-        elif hdd_block_max == hdd2_block_last and not node.hyper_recompress:
-            node.logger.app_log.warning("Status: Hyperblock recompression skipped")
-            node.recompress = False
-        else:
-            lowest_block = min(hdd_block_max, hdd2_block_last, hdd_block_max_diff, hdd2_block_last_misc)
-            highest_block = max(hdd_block_max, hdd2_block_last, hdd_block_max_diff, hdd2_block_last_misc)
-
-            node.logger.app_log.warning(
-                f"Status: Cross-integrity check failed, {highest_block} will be rolled back below {lowest_block}")
-
-            rollback(node,db_handler_initial,lowest_block) #rollback to the lowest value
-            node.recompress = False
-
-    else:
-        node.logger.app_log.warning("Status: Compressing ledger to Hyperblocks")
-        node.recompress = True
-
-
-
-
-def bin_convert(string):
-    # TODO: Move to essentials.py
-    return ''.join(format(ord(x), '8b').replace(' ', '0') for x in string)
-
-
-def balanceget(balance_address, db_handler):
-    # Get mempool transactions for the address
-    base_mempool = mp.MEMPOOL.mp_get(balance_address)
-    debit_mempool = Decimal("0")
-
-    # Calculate mempool fees and debit
-    if base_mempool:
-        for tx in base_mempool:
-            debit_tx = Decimal(tx[0])
-            fee = fee_calculate(tx[1], tx[2], node.last_block)
-            debit_mempool += quantize_eight(debit_tx + fee)
-
-    # Use a single query to get ledger balances
-    query = """
-        SELECT 
-            COALESCE(SUM(CASE WHEN recipient = ? THEN amount ELSE 0 END), 0) as credit,
-            COALESCE(SUM(CASE WHEN address = ? THEN amount ELSE 0 END), 0) as debit,
-            COALESCE(SUM(CASE WHEN address = ? THEN fee ELSE 0 END), 0) as fees,
-            COALESCE(SUM(CASE WHEN recipient = ? THEN reward ELSE 0 END), 0) as rewards
-        FROM transactions
-        WHERE recipient = ? OR address = ?
-    """
-
-    try:
-        db_handler.execute_param(db_handler.h, query,
-                                 (balance_address, balance_address, balance_address,
-                                  balance_address, balance_address, balance_address))
-        result = db_handler.h.fetchone()
-        credit_ledger, debit_ledger, fees, rewards = map(Decimal, result)
-    except Exception:
-        credit_ledger = debit_ledger = fees = rewards = Decimal("0")
-
-    # Calculate balances
-    balance = quantize_eight(credit_ledger - debit_ledger - fees + rewards - debit_mempool)
-    balance_no_mempool = quantize_eight(credit_ledger - debit_ledger - fees + rewards)
-
-    return (
-        str(balance),
-        str(credit_ledger),
-        str(debit_ledger + debit_mempool),
-        str(fees),
-        str(rewards),
-        str(balance_no_mempool)
-    )
-
-
-# init
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -579,7 +335,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     block_hash_delete = receive(self.request)
                     # print peer_ip
                     if consensus_blockheight == node.peers.consensus_max:
-                        blocknf(node, block_hash_delete, peer_ip, db_handler_instance, mp=mp)
+                        blocknf(node, block_hash_delete, peer_ip, db_handler_instance, mp=mp, tokens=tokens)
                         if node.peers.warning(self.request, peer_ip, "Rollback", 2):
                             node.logger.app_log.info(f"{peer_ip} banned")
                             break
@@ -593,7 +349,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     block_hash_delete = receive(self.request)
                     # print peer_ip
                     if consensus_blockheight == node.peers.consensus_max:
-                        blocknf(node, block_hash_delete, peer_ip, db_handler_instance, hyperblocks=True, mp=mp)
+                        blocknf(node, block_hash_delete, peer_ip, db_handler_instance, hyperblocks=True, mp=mp, tokens=tokens)
                         if node.peers.warning(self.request, peer_ip, "Rollback", 2):
                             node.logger.app_log.info(f"{peer_ip} banned")
                             break
@@ -762,7 +518,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     if node.peers.is_allowed(peer_ip, data):
                         balance_address = receive(self.request)  # for which address
 
-                        balanceget_result = balanceget(balance_address, db_handler_instance)
+                        balanceget_result = balanceget(balance_address, db_handler_instance, mp, node)
 
                         send(self.request,
                                          balanceget_result)  # return balance of the address to the client, including mempool
@@ -775,7 +531,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     if node.peers.is_allowed(peer_ip, data):
                         balance_address = receive(self.request)  # for which address
 
-                        balanceget_result = balanceget(balance_address, db_handler_instance)
+                        balanceget_result = balanceget(balance_address, db_handler_instance, mp, node)
                         response = {"balance": balanceget_result[0],
                                     "credit": balanceget_result[1],
                                     "debit": balanceget_result[2],
@@ -794,7 +550,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     if node.peers.is_allowed(peer_ip, data):
                         balance_address = receive(self.request)  # for which address
 
-                        balanceget_result = balanceget(balance_address, db_handler_instance)[0]
+                        balanceget_result = balanceget(balance_address, db_handler_instance, mp, node)[0]
 
                         send(self.request,
                                          balanceget_result)  # return balance of the address to the client, including mempool
@@ -806,7 +562,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     if node.peers.is_allowed(peer_ip, data):
                         balance_address = receive(self.request)  # for which address
 
-                        balanceget_result = balanceget(balance_address, db_handler_instance)
+                        balanceget_result = balanceget(balance_address, db_handler_instance, mp, node)
                         response = {"balance": balanceget_result[0]}
 
                         send(self.request,
@@ -1553,7 +1309,7 @@ def initial_db_check(node):
     if os.path.exists("fresh_sync") and node.is_mainnet:
         node.logger.app_log.warning("Status: Fresh sync required, bootstrapping from the website")
         os.remove("fresh_sync")
-        bootstrap()
+        bootstrap(node)
     # UPDATE mainnet DB if required
     if node.is_mainnet:
         upgrade = sqlite3.connect(node.ledger_path)
@@ -1570,7 +1326,7 @@ def initial_db_check(node):
             print(e)
             upgrade.close()
             print("Database needs upgrading, bootstrapping...")
-            bootstrap()
+            bootstrap(node)
 
 
 def load_keys(node):
@@ -1682,7 +1438,7 @@ if __name__ == "__main__":
             node.apihandler = apihandler.ApiHandler(node.logger.app_log, config)
             mp.MEMPOOL = mp.Mempool(node.logger.app_log, config, node.db_lock, node.is_testnet, trace_db_calls=node.trace_db_calls)
 
-            check_integrity(node.hyper_path)
+            check_integrity(node.hyper_path, node)
             #PLACEHOLDER FOR FRESH HYPERBLOCK BUILDER
 
             # if node.rebuild_db: #does nothing
--- worker.py
+++ worker.py
@@ -228,7 +228,7 @@ def worker(host, port, node):
                 # if max(consensus_blockheight_list) == int(received_block_height):
                 if int(received_block_height) == node.peers.consensus_max:
 
-                    blocknf(node, block_hash_delete, peer_ip, db_handler_instance, hyperblocks=True)
+                    blocknf(node, block_hash_delete, peer_ip, db_handler_instance, hyperblocks=True, mp=mp)
 
                     if node.peers.warning(s, peer_ip, "Rollback", 2):
                         raise ValueError(f"{peer_ip} is banned")
